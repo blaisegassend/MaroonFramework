5745664d7f87778524d6d621bb052ee6944dc8ff
Ryan Cahoon <ryan.m.cahoon@gmail.com>
2020-10-04 19:18:55 +0000

    Split arcade drive tutorial code

--- src/main/java/com/team766/robot/OI.java
+++ src/main/java/com/team766/robot/OI.java
@@ -29,7 +29,7 @@ public class OI extends Procedure {
 		while (true) {
 			// Add driver controls here - make sure to take/release ownership
 			// of mechanisms when appropriate.
-			Robot.drive.setArcadeDrivePower(joystick0.getAxis(1), joystick0.getAxis(0));
+			Robot.drive.setArcadeDrivePower(joystick0.getAxis(1), joystick1.getAxis(0));
 
 			context.waitFor(() -> RobotProvider.instance.hasNewDriverStationData());
 		}
